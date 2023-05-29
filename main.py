a0ae9f95ea1b86451ce427a8a1a632ea2ead8c8e
Ilya <pulinilija@gmail.com>
2023-05-29 09:39:08 +0700

    task2

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-# Задача 16: Требуется вычислить, сколько раз встречается некоторое число X в массиве
-# A[1..N]. Пользователь в первой строке вводит натуральное число N – количество элементов
-# в массиве. В последующих  строках записаны N целых чисел Ai. Последняя строка содержит
-# число X
+# Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному
+# числу X. Пользователь в первой строке вводит натуральное число N – количество элементов в
+# массиве. В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
 # *Пример:*
+
 # 5
 #     1 2 3 4 5
-#     3
-#     -> 1
+#     6
+#     -> 5
 
 n = int(input("Введите количетсво чисел: "))
-list_1 = [int(input("Введите число до {n}: ")) for i in range(n)]
+list_1 = [int(input("Введите число: ")) for i in range(n)]
 print(*list_1)
 
 x = int(input("Введите x: "))
-count = 0
-for i in range(n):
-    if list_1[i] == x:
-        count+=1
-print(count)
+list_1.append(x)
+min_diff = 100
+low_position = 0
+high_position = 0
+for i in range(n+1):
+    if list_1[-1] > list_1[i] and list_1[-1] - list_1[i] <= min_diff:
+        min_diff = list_1[-1] - list_1[i]
+        low_position = list_1[i]
+    elif list_1[-1] < list_1[i] and list_1[i] - list_1[-1] <= min_diff:
+        min_diff = list_1[i] - list_1[-1]
+        high_position = list_1[i]
+
+if low_position != 0:
+    print(low_position)
+if high_position != 0:
+    print(high_position)
